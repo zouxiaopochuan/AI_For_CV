e5bd88734ebcfebc8c64dc6c7481b49a1afd21b9
xiaopochuan <574729272@qq.com>
2019-04-26 17:42:21 +0800

    图像的线检测和圆检测

---
+++ HoughCirles.cpp
@@ -0,0 +1,32 @@
+﻿#include<opencv2\opencv.hpp>
+#include<opencv2\highgui\highgui.hpp>
+#include<opencv2\imgproc\imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv) {
+	Mat image = imread("circle.jpg");
+	imshow("原始图", image);
+
+	Mat grayImage;
+	//cvtColor(image, grayImage, COLOR_GRAY2BGR);
+	Canny(image, grayImage, 50, 200, 3);
+	GaussianBlur(grayImage, grayImage, Size(9, 9), 2, 2);
+	
+	
+	vector<Vec3f> circles;
+	HoughCircles(grayImage, circles, HOUGH_GRADIENT, 1.5, grayImage.rows/5, 150, 100, 0, 110);
+
+	
+	for (size_t i = 0; i < circles.size(); i++) {
+		Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+
+		int radius = cvRound(circles[i][2]);
+		circle(image, center, 3, Scalar(0, 255, 0), -1, 8, 0);
+		circle(image, center, radius, Scalar(155, 50, 255), 3, 8, 0);
+	}
+	imshow("边缘检测后的图", image);
+	waitKey();
+	return 0;
+}
---
+++ HoughLinesP.cpp
@@ -0,0 +1,30 @@
+﻿#include<opencv2\opencv.hpp>
+#include<opencv2\highgui\highgui.hpp>
+#include<opencv2\imgproc\imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv) {
+	Mat image = imread("line.jpg");
+	imshow("原始图", image);
+
+	Mat midImage, dstImage;
+	Canny(image, midImage, 100, 150, 3);
+	cvtColor(midImage, dstImage, COLOR_GRAY2BGR);
+	
+	//存放的是线的极坐标表示 r,theta
+	vector<Vec4i> lines;
+	HoughLinesP(midImage, lines, 1, CV_PI / 180, 30, 40, 5);
+
+	for (size_t i = 0; i < lines.size(); i++) {
+		Vec4i l = lines[i];
+		line(dstImage, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(186, 88, 255), 1, LINE_AA);
+		
+	}
+	imshow("边缘检测后的图", midImage);
+	imshow("最终效果图", dstImage);
+
+	waitKey();
+	return 0;
+}
