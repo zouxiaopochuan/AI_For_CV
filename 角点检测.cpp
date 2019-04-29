2fd68d5849d74b22499ea2a87428a59efb0d0f0a
xiaopochuan <574729272@qq.com>
2019-04-29 16:10:10 +0800

    轮框检测和角点检测

---
+++ 角点检测.cpp
@@ -0,0 +1,56 @@
+﻿#include<opencv2\opencv.hpp>
+#include<opencv2\highgui\highgui.hpp>
+#include<opencv2\imgproc\imgproc.hpp>
+#include<iostream>
+
+using namespace std;
+using namespace cv;
+
+Mat g_srcImage, g_srcImage1, g_grayImage;
+int thresh = 70;
+int max_thresh = 175;
+
+void on_CornerHarris(int, void*);
+
+int main(int argc, char** argv) {
+	g_srcImage = imread("lena.jpg");
+
+	imshow("原始图", g_srcImage);
+	g_srcImage1 = g_srcImage.clone();
+
+	cvtColor(g_srcImage1, g_grayImage, COLOR_BGR2GRAY);
+
+	namedWindow("角点检测", WINDOW_AUTOSIZE);
+	createTrackbar("阈值：", "角点检测", &thresh, max_thresh, on_CornerHarris);
+
+	on_CornerHarris(0, 0);
+
+	waitKey(0);
+	return 0;
+}
+
+void on_CornerHarris(int, void*) {
+	Mat dstImage;
+	Mat normImage;
+	Mat scaledImage;
+	
+	dstImage = Mat::zeros(g_srcImage.size(), CV_32FC1);
+	g_srcImage1 = g_srcImage.clone();
+
+	//进行角点检测
+	//第三个参数表示领域的大小，第四个参数辨识sobel算子孔径大小，第五个表示Harris参数
+	cornerHarris(g_grayImage, dstImage, 2, 3, 0.04, BORDER_DEFAULT);
+	normalize(dstImage, normImage, 0, 255, NORM_MINMAX, CV_32FC1, Mat());
+	convertScaleAbs(normImage, scaledImage);
+
+	for (int j = 0; j < normImage.rows; j++) {
+		for (int i = 0; i < normImage.cols; i++) {
+			if ((int)normImage.at<float>(j, i) > thresh + 80) {
+				circle(g_srcImage1, Point(i, j), 5, Scalar(10, 10, 255), 2, 8, 0);
+				circle(scaledImage, Point(i, j), 5, Scalar(0, 10, 255), 2, 8, 0);
+			}
+		}
+	}
+	imshow("角点检测", g_srcImage1);
+	
+}
---
+++ 轮框图.cpp
@@ -0,0 +1,41 @@
+﻿#include<opencv2\opencv.hpp>
+#include<opencv2\highgui\highgui.hpp>
+#include<opencv2\imgproc\imgproc.hpp>
+#include<iostream>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv) {
+	Mat image = imread("circle.jpg");
+	namedWindow("My original");
+	imshow("My original", image);
+	Mat gray;
+	cvtColor(image, gray, COLOR_RGB2GRAY);
+	
+	int thresh_size = (100 / 4) * 2 + 1;
+	adaptiveThreshold(gray, gray, 255, ADAPTIVE_THRESH_GAUSSIAN_C,
+		THRESH_BINARY_INV, thresh_size, thresh_size / 3);
+
+	imshow("gray", gray);
+	vector<vector<Point>> contours;
+	findContours(gray, contours, RETR_EXTERNAL, CHAIN_APPROX_NONE);
+	vector<vector<Point>> contours1;
+	for (int i = 0; i < contours.size(); i++) {
+		contours1.push_back(contours[i]);
+	}
+
+	Mat hole(gray.size(), CV_8U, Scalar(0));
+	drawContours(hole, contours1, -1, Scalar(255), FILLED);
+	namedWindow("My hole");
+	imshow("My hole", hole);
+
+	Mat crop(image.rows, image.cols, CV_8UC3);
+	image.copyTo(crop, hole);
+	namedWindow("My warpPerspective");
+	imshow("My warpPerspective", crop);
+
+	waitKey();
+
+	return 0;
+}
